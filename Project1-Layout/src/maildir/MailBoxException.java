026a84243556e3c8061ee9759a957e204211045e
AdamWalton-0 <waltona@merrimack.edu>
2025-09-30 18:32:52 -0400

    Delete Project1-Layout directory

--- Project1-Layout/src/maildir/MailBoxException.java
+++
@@ -1,7 +0,0 @@
-package maildir;
-
-/** Thin wrapper for all mailbox-related errors. */
-public class MailBoxException extends Exception {
-    public MailBoxException(String msg) { super(msg); }
-    public MailBoxException(String msg, Throwable cause) { super(msg, cause); }
-}
