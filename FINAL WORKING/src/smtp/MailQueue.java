23d076b2dd27633955d4b658998b724862da916b
AdamWalton-0 <waltona@merrimack.edu>
2025-10-01 18:23:37 -0400

    Update MailQueue.java

--- FINAL WORKING/src/smtp/MailQueue.java
+++ FINAL WORKING/src/smtp/MailQueue.java
@@ -4,9 +4,10 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import maildir.MailMessage;
 
-/** Simple message queue. */
+
 public class MailQueue {
     private final BlockingQueue<MailMessage> q = new LinkedBlockingQueue<>();
     public void put(MailMessage m) { if (m != null) q.offer(m); }
     public MailMessage take() throws InterruptedException { return q.take(); }
 }
+
