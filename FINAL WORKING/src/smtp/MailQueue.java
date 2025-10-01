6fa01e277e8eefb5322c6d6f61d87a787637e646
AdamWalton-0 <waltona@merrimack.edu>
2025-10-01 18:53:26 -0400

    Update MailQueue.java
    ADDED COMMENTS

--- FINAL WORKING/src/smtp/MailQueue.java
+++ FINAL WORKING/src/smtp/MailQueue.java
@@ -4,10 +4,12 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import maildir.MailMessage;
 
-
+//Thread safe queue for mailmessage
 public class MailQueue {
+//Queue to hold mail messages objects
     private final BlockingQueue<MailMessage> q = new LinkedBlockingQueue<>();
+//Add message to the queue if not null
     public void put(MailMessage m) { if (m != null) q.offer(m); }
+//Take and remove message from the queue block if empty
     public MailMessage take() throws InterruptedException { return q.take(); }
 }
-
