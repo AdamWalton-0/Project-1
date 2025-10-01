da990231c1b00c7041258d0bcd409ecf5c2c0767
AdamWalton-0 <waltona@merrimack.edu>
2025-10-01 18:24:55 -0400

    Delete Project1-Layout directory

--- Project1-Layout/src/pop3/POP3Exception.java
+++
@@ -1,7 +0,0 @@
-package pop3;
-
-/** Simple checked exception for POP3-specific failures. */
-public class POP3Exception extends Exception {
-    public POP3Exception(String msg) { super(msg); }
-    public POP3Exception(String msg, Throwable cause) { super(msg, cause); }
-}
