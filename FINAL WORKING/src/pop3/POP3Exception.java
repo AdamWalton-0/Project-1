8c644e2b0a041751b0cc5a1b15ecbfc1db2e5d8b
AdamWalton-0 <waltona@merrimack.edu>
2025-10-01 22:34:39 -0400

    Update POP3Exception.java

--- FINAL WORKING/src/pop3/POP3Exception.java
+++ FINAL WORKING/src/pop3/POP3Exception.java
@@ -1,7 +1,10 @@
 package pop3;
 
-/** Simple checked exception for POP3-specific failures. */
+//exeption for POP3 errors
 public class POP3Exception extends Exception {
+//Makes a POP3 error with a message
     public POP3Exception(String msg) { super(msg); }
+//nested error
     public POP3Exception(String msg, Throwable cause) { super(msg, cause); }
+
 }
