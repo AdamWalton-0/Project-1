21a58035ca3b94b74f250e13ae4501269f6bf2cc
AntonRyan1 <ryanan@merrimack.edu>
2025-10-01 23:03:27 -0400

    Update MailBoxException.java

--- FINAL WORKING/src/maildir/MailBoxException.java
+++ FINAL WORKING/src/maildir/MailBoxException.java
@@ -1,7 +1,6 @@
 package maildir;
 
-/** Thin wrapper for all mailbox-related errors. */
-public class MailBoxException extends Exception {
-    public MailBoxException(String msg) { super(msg); }
-    public MailBoxException(String msg, Throwable cause) { super(msg, cause); }
+public class MailBoxException extends Exception {// custom exeption if something goes wrong within the mailbox
+    public MailBoxException(String msg) { super(msg); }// constuctor that makes messages for ex "spool not found"/ also sends message to exeption class
+    public MailBoxException(String msg, Throwable cause) { super(msg, cause); }// constructor, takes a messsage aswell as og error.
 }
